1a034ed84c06493d0eb7edbd66b6ba053048e8ab
ysj <593380806@qq.com>
2019-01-18 17:00:29 +0800

    增加log规范案例

--- ayc/Test/src/main/java/com/ayc/controller/TestController.java
+++ ayc/Test/src/main/java/com/ayc/controller/TestController.java
@@ -1,10 +1,14 @@
 
 package com.ayc.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.ayc.configs.AppConfigs;
+import com.ayc.entity.TestEntity;
 import com.ayc.framework.common.AjaxResult;
 import com.ayc.framework.common.BizException;
 import com.ayc.service.TestSevice;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +24,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/test")
 public class TestController {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     private TestSevice testSevice;
     @Autowired
@@ -27,10 +32,23 @@ public class TestController {
     @GetMapping("/list")
     @ResponseBody
     public AjaxResult login(String mobile, String password) {
+        long startTime = System.currentTimeMillis();
+        logger.info("接口调用开始,参数:[mobile:{},password:{}]", mobile, password);
+        TestEntity testEntity = null;
         try {
-            return AjaxResult.success(testSevice.reTest(appConfigs.getBizdbIndex()));
-        } catch (BizException ex) {
-            return AjaxResult.failed(ex.getCode());
+            testEntity = testSevice.reTest(appConfigs.getBizdbIndex());
+        } catch (BizException e) {
+            long endTime = System.currentTimeMillis();
+            logger.error("接口调用异常!耗时:{},异常信息:{}", (endTime - startTime), e.getMessage(), e);
+            return AjaxResult.failed();
+        } catch (Exception e) {
+            // 记录异常信息
+            long endTime = System.currentTimeMillis();
+            logger.error("接口调用异常!耗时:{},异常信息:{}", (endTime - startTime), e.getMessage(), e);
+            return AjaxResult.failed();
         }
+        long endTime = System.currentTimeMillis();
+        logger.info("接口调用完成,耗时:{},返回结果:[testEntity:{}]", (endTime - startTime), testEntity);
+        return AjaxResult.success(testEntity);
     }
 }

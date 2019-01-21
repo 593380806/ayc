fe93bb80d205457bddaaa1d3761683ecaee48495
ysj <593380806@qq.com>
2019-01-21 10:53:41 +0800

    表单验证案例

--- ayc/Test/src/main/java/com/ayc/controller/TestController.java
+++ ayc/Test/src/main/java/com/ayc/controller/TestController.java
@@ -4,7 +4,10 @@ package com.ayc.controller;
 import com.alibaba.fastjson.JSON;
 import com.ayc.configs.AppConfigs;
 import com.ayc.entity.TestEntity;
+import com.ayc.framework.aspect.ValidateFiled;
+import com.ayc.framework.aspect.ValidateGroup;
 import com.ayc.framework.common.AjaxResult;
+import com.ayc.framework.common.BizCode;
 import com.ayc.framework.common.BizException;
 import com.ayc.service.TestSevice;
 import org.slf4j.Logger;
@@ -31,7 +34,13 @@ public class TestController {
     private AppConfigs appConfigs;
     @GetMapping("/list")
     @ResponseBody
-    public AjaxResult login(String mobile, String password) {
+    @ValidateGroup(fileds = { @ValidateFiled(index = 0, notNull = true, icode = BizCode.CLIENT_ERROR, message = "版本号"),
+            @ValidateFiled(index = 1, notNull = true, icode = BizCode.CLIENT_ERROR, message = "电话"),
+            @ValidateFiled(index = 2, notNull = true, icode = BizCode.CLIENT_ERROR, message = "密码"),
+             })
+    //@ValidateFiled(index = 3, notNull = true, icode = BizCode.CLIENT_ERROR, message = "表单"),
+    //@ValidateFiled(index = 3, filedName = "id", notNull = true, icode = BizCode.CLIENT_ERROR, message = "id"),
+    public AjaxResult login(String mobile, String password/* ,TestEntity t */) {
         long startTime = System.currentTimeMillis();
         logger.info("接口调用开始,参数:[mobile:{},password:{}]", mobile, password);
         TestEntity testEntity = null;
